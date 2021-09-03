46f928bef6f90091263cb4cc0c6a0d4ef81c304f
TauH <thaywood988@gmail.com>
2021-09-03 15:17:53 +1000

    added checkpoint if, added arrays

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(',  '}', '{', ']', '[', '~', '-', '_', '.',];
+var lowerCasedCharacters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+function generatePassword(){
+
+  var passLength = prompt("How long would you like your password?", "enter length here");
+
+    //checks requested password parameters are good.
+  if (8 > passLength &&
+    passLength > 128 &&
+    !isNaN(passLength) ){
+      alert("incorrect input, you must put in a number between 8 and 128.")
+      return;
+  }
+
+
+  
+}
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
+
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +37,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
