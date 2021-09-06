d84213f2209c1f37e1eecd83dc82eeca87f4ae79
TauH <thaywood988@gmail.com>
2021-09-06 18:58:50 +1000

    fixed undifined output bug

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Created a password generator. With options for what characters you want to use.
 Has a character limit of between 8 and 128 characters.
 If you enter a incorrect input it will allow you to try again.
+
+"(7hx9}PgF1*Y(,9el6MZ83Hs2Abz(f/?U_y/^XL8C9fjW0xM^-TqD.a%UHJWpzAjnFk0639x?wq5r*({iu8Z$aJOJ$3:t%0$9m1[L!TA{$+nT?6-Zfs?6_6g99#"
+
+"ruXMC07F~:R_b3C(K06'fMF9+^]6+#x1a'J0$X(6~WE?Tkrud93DBIp#)jy_1)9i3!9#q8g{BU@,4!d{U})~enp?a4'dZ66cU7t5TO_M02#z63XaG@'2~-]#48^"
--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 // Assignment Code
+var passwordElement = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
-var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(',  '}', '{', ']', '[', '~', '-', '_', '.',];
-var lowerCasedCharacters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
-var upperCasedCharacters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+var specialCharacters = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(',  '}', '{', ']', '[', '~', '-', '_', '.'];
+var lowerCasedCharacters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCasedCharacters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 //want to return it our of function its in
-/*function playAgain() {
-  alert("incorrect input, you must put in a number between 8 and 128.");
-    var again = confirm("Would you like to try again?");
-    if (again){
-      generatePassword();
-    } 
-    return;
-}*/
+// function playAgain() {
+//   alert("incorrect input, you must put in a number between 8 and 128.");
+//     var again = confirm("Would you like to try again?");
+//     if (again){
+//       generatePassword();
+//     } 
+//     return ;
+// }
 
 function generatePassword(){
   var options = [];
-  var optionsLength = [];
-  var password ="";
+  var password = "";
   var passLength = prompt("How long would you like your password?", "enter length here");
 
    //checks if input is valid.
   if (isNaN(passLength)){
     alert("incorrect input, you must put in a number between 8 and 128.");
-    var playAgain = confirm("Would you like to try again?");
-    if (playAgain){
-      generatePassword();
-    } 
-    return;
+     var playAgain = confirm("Would you like to try again?");
+     if (playAgain){
+       generatePassword();
+     } 
+     return;
   } else {
     passLength = parseInt(passLength);
     if (passLength < 8 || passLength > 128){
@@ -44,22 +45,18 @@ function generatePassword(){
   var wantsSpecial = confirm("Do you want to use special characters in you password?");
   if(wantsSpecial){
     options.push(specialCharacters);
-    optionsLength.push(specialCharacters.length);
   }
   var wantsLowerC = confirm("Do you want to use lowercase letters in you password?");
   if(wantsLowerC){
     options.push(lowerCasedCharacters);
-    optionsLength.push(lowerCasedCharacters.length);
   }
   var wantsUpperC = confirm("Do you want to use uppercase letters in you password?");
   if(wantsUpperC){
     options.push(upperCasedCharacters);
-    optionsLength.push(upperCasedCharacters.length);
   }
   var wantsNumbers = confirm("Do you want to use numbers in you password?");
   if(wantsNumbers){
     options.push(numericCharacters);
-    optionsLength.push(numericCharacters.length);
   }
 
   //loops user back to start if no characters selected
@@ -75,27 +72,29 @@ function generatePassword(){
       return;
     
     }
+    
   //loop to make password
   for(let i = 0; i < passLength; i++){
     var rng = Math.floor(Math.random()*options.length);
-    var temp = options[rng][Math.floor(Math.random()*optionsLength[rng])];
+    var temp = options[rng][Math.floor(Math.random()*options[rng].length)];
     password += temp;
   }
+
+  console.log('password',password);
   alert(password);
   return password;
 }
 
 // Write password to the #password input
 function writePassword() {
-
   
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  if (password == undefined){
+    debugger
+  }
+  passwordElement.value = password;
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
